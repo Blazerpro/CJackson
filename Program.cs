ec8658086a0a23e5ea6e07efc79cfd682e739b11
Armand <armandbernard1995@gmail.com>
2022-07-21 20:53:40 +0100

    exponent function

--- Program.cs
+++ Program.cs
@@ -3,27 +3,20 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        while (true)
+        var firstnum = 4;
+        var exponent = 3;
+
+        var answer = ExponentFunc(firstnum, exponent);
+        Console.WriteLine(answer);
+    }
+
+    private static int ExponentFunc(int initial, int exponent)
+    {
+        var answer = initial;
+        for (int i = 1; i < exponent; i++)
         {
-            Console.WriteLine("Give me a number:");
-
-            var num1 = Console.ReadLine();
-
-            Console.WriteLine("Give me a second number:");
-
-            var num2 = Console.ReadLine();
-
-            int total = 1;
-
-            if (int.TryParse(num1, out var num1int) == true && int.TryParse(num2, out var num2int) == true)
-            {
-                total = num1int + num2int;
-                Console.WriteLine($"The sum of your numbers is {total}");
-            }
-            else
-            {
-                Console.WriteLine("You are an idiot, please try using numbers this time!");
-            }
+            answer = answer * initial;
         }
+        return answer;
     }
 }
