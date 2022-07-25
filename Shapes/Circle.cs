e8b9aa13185871e639057a517f8b2ca5bc032148
Armand <armandbernard1995@gmail.com>
2022-07-25 20:37:55 +0100

    Add circle challenge spec

---
+++ Shapes/Circle.cs
@@ -0,0 +1,5 @@
+/// SPEC
+/// Create a class called "Circle" with Methods "Perimeter" and "Area"
+/// In the constructor, you should provide any dimensions needed to calculate these.
+/// These dimensions should be stored in "properties", and should not be set-able from the outside.
+/// They should, however, be get-able from the outside.
