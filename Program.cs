2854a08ff134aace856a658858440ad7ed873dc2
Blazerpro <cameronjackson173@gmail.com>
2022-07-25 19:19:00 +0100

    Fix for negative exponents

--- Program.cs
+++ Program.cs
@@ -4,19 +4,20 @@ public class Program
     public static void Main(string[] args)
     {
         var firstnum = 4;
-        var exponent = 3;
+        var exponent = -2;
 
         var answer = ExponentFunc(firstnum, exponent);
         Console.WriteLine(answer);
     }
 
-    private static int ExponentFunc(int initial, int exponent)
+    private static double ExponentFunc(int initial, int exponent)
     {
-        var answer = 1;
-        for (int i = 0; i < exponent; i++)
+        var answer = 1.0;
+        var counter = Math.Abs(exponent);
+        for (int i = 0; i < counter; i++)
         {
             //              logical expression ? value if true : value if false
-            answer = answer * ((exponent < 0) ? (1 / initial) : (initial));
+            answer = answer * ((exponent < 0) ? (1.0 / initial) : (initial));
         }
         return answer;
     }
