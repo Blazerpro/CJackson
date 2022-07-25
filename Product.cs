66b5c7ee84bc5ae791036c7defea1439c19ac701
Armand <armandbernard1995@gmail.com>
2022-07-25 20:08:12 +0100

    property with backing field

--- Product.cs
+++ Product.cs
@@ -4,14 +4,31 @@ public class Product : object
 {
     public string Name;
 
+    // backing field
+    private int _Version;
+
+    // Property
+    public int Version
+    {
+        get
+        {
+            return _Version + 1;
+        }
+        private set
+        {
+            _Version = value;
+        }
+    }
+
     public override string ToString()
     {
         return ProductNameAppender();
     }
 
-    public Product(string name)
+    public Product(string name, int version)
     {
         Name = name;
+        Version = version;
     }
 
     public string ProductNameAppender()
--- Program.cs
+++ Program.cs
@@ -5,17 +5,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        object[] objects = { new Product("iTunes"), 1, "Hi" };
+        object[] objects = { new Product("iTunes", 1), 1, "Hi" };
 
-        var iTunes = new Product("iTunes");
-        var chrome = new Product("iChrome");
+        var iTunes = new Product("iTunes", 1);
 
-        iTunes.ProductNameAppenderExtension();
-
-        Console.WriteLine(iTunes.ProductNameAppender());
-        Console.WriteLine(Product.ProductNameAppender(iTunes));
-        Console.WriteLine(chrome.ProductNameAppender());
-
-        objects.PrintAll();
+        Console.WriteLine(iTunes.Version);
     }
 }
