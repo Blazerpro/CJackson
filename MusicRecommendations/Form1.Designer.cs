4265b7b6979efdc00538aa524c8e43bb59c59bd0
Blazerpro <cameronjackson173@gmail.com>
2022-08-01 21:16:36 +0100

    Programmed RowAdd logic

--- MusicRecommendations/Form1.Designer.cs
+++ MusicRecommendations/Form1.Designer.cs
@@ -31,10 +31,11 @@
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TitleBox = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.AristOrTrackBox = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ListTLP = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -46,6 +47,7 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ListTLP, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 10);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(10);
@@ -63,10 +65,10 @@
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanel3.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.TitleBox, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.textBox2, 1, 0);
             this.tableLayoutPanel3.Controls.Add(this.AristOrTrackBox, 2, 0);
-            this.tableLayoutPanel3.Controls.Add(this.button1, 3, 0);
+            this.tableLayoutPanel3.Controls.Add(this.AddButton, 3, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
@@ -75,14 +77,14 @@
             this.tableLayoutPanel3.Size = new System.Drawing.Size(791, 54);
             this.tableLayoutPanel3.TabIndex = 0;
             // 
-            // textBox1
+            // TitleBox
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(3, 15);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PlaceholderText = "Title";
-            this.textBox1.Size = new System.Drawing.Size(372, 23);
-            this.textBox1.TabIndex = 0;
+            this.TitleBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.TitleBox.Location = new System.Drawing.Point(3, 15);
+            this.TitleBox.Name = "TitleBox";
+            this.TitleBox.PlaceholderText = "Title";
+            this.TitleBox.Size = new System.Drawing.Size(372, 23);
+            this.TitleBox.TabIndex = 0;
             // 
             // textBox2
             // 
@@ -96,6 +98,7 @@
             // AristOrTrackBox
             // 
             this.AristOrTrackBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.AristOrTrackBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.AristOrTrackBox.FormattingEnabled = true;
             this.AristOrTrackBox.Items.AddRange(new object[] {
             "Album",
@@ -105,18 +108,32 @@
             this.AristOrTrackBox.Size = new System.Drawing.Size(94, 23);
             this.AristOrTrackBox.TabIndex = 2;
             // 
-            // button1
+            // AddButton
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.AddButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(733, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(55, 48);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.AddButton.Location = new System.Drawing.Point(733, 3);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(55, 48);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ListTLP
+            // 
+            this.ListTLP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListTLP.ColumnCount = 1;
+            this.ListTLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ListTLP.Location = new System.Drawing.Point(3, 63);
+            this.ListTLP.Name = "ListTLP";
+            this.ListTLP.RowCount = 1;
+            this.ListTLP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ListTLP.Size = new System.Drawing.Size(791, 382);
+            this.ListTLP.TabIndex = 1;
             // 
             // tableLayoutPanel2
             // 
@@ -154,10 +171,11 @@
         private ColorDialog colorDialog1;
         private TableLayoutPanel tableLayoutPanel1;
         private TableLayoutPanel tableLayoutPanel3;
-        private TextBox textBox1;
+        private TextBox TitleBox;
         private TextBox textBox2;
         private ComboBox AristOrTrackBox;
-        private Button button1;
+        private Button AddButton;
         private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel ListTLP;
     }
 }
--- MusicRecommendations/Form1.cs
+++ MusicRecommendations/Form1.cs
@@ -8,11 +8,21 @@ namespace MusicRecommendations
             AristOrTrackBox.SelectedIndex = 0;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void AddButton_Click(object sender, EventArgs e)
         {
-
+            var item = CreateItem();
+            ListTLP.RowStyles.Insert(ListTLP.RowCount-1, new RowStyle(SizeType.AutoSize));
+            ListTLP.RowCount++;
+            ListTLP.Controls.Add(item,0,ListTLP.RowCount-1);
         }
 
+        private Control CreateItem()
+        {
+            var item = new Label();
+            item.Text = TitleBox.Text;
+            return item;
+            // TODO: Create a proper row item, using TableLayoutPanel with actual columns etc.
+        }
 
     }
 }
