3efd8e15dcf06cbf86b0901e462983160aa88b80
Blazerpro <cameronjackson173@gmail.com>
2022-08-01 20:43:51 +0100

    First iteration of UI and coding

--- MusicRecommendations/Form1.cs
+++ MusicRecommendations/Form1.cs
@@ -6,5 +6,12 @@ namespace MusicRecommendations
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
