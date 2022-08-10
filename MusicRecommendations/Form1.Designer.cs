8b35eedf6f987163ecca9251c8b8d66988a126e5
Armand <armandbernard1995@gmail.com>
2022-08-10 19:07:47 +0100

    Make list scrollable

--- MusicRecommendations/Form1.Designer.cs
+++ MusicRecommendations/Form1.Designer.cs
@@ -35,10 +35,12 @@
             this.ArtistBox = new System.Windows.Forms.TextBox();
             this.ArtistOrTrackBox = new System.Windows.Forms.ComboBox();
             this.AddButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.ListTLP = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,12 +49,12 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.ListTLP, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 10);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(10);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(797, 448);
@@ -121,18 +123,27 @@
             this.AddButton.UseVisualStyleBackColor = true;
             this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.ListTLP);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(791, 382);
+            this.panel1.TabIndex = 2;
+            // 
             // ListTLP
             // 
-            this.ListTLP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListTLP.AutoSize = true;
             this.ListTLP.ColumnCount = 1;
             this.ListTLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.ListTLP.Location = new System.Drawing.Point(3, 63);
+            this.ListTLP.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ListTLP.Location = new System.Drawing.Point(0, 0);
             this.ListTLP.Name = "ListTLP";
             this.ListTLP.RowCount = 1;
             this.ListTLP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.ListTLP.Size = new System.Drawing.Size(791, 382);
+            this.ListTLP.Size = new System.Drawing.Size(791, 0);
             this.ListTLP.TabIndex = 1;
             // 
             // tableLayoutPanel2
@@ -161,6 +172,8 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel3.ResumeLayout(false);
             this.tableLayoutPanel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -177,5 +190,6 @@
         private Button AddButton;
         private TableLayoutPanel tableLayoutPanel2;
         private TableLayoutPanel ListTLP;
+        private Panel panel1;
     }
 }
